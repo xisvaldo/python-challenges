696925369ab697ecae7bf5f2a45481cc830e4207
Leonardo Borges <leonardo.xis.borges@gmail.com>
2026-03-17 16:22:34 +0100

    chore: added unit tests for file_reader service

---
+++ log_analyzer/tests/log_analyzer/services/test_file_reader.py
@@ -0,0 +1,33 @@
+import unittest
+from pathlib import Path
+from unittest.mock import patch
+
+from log_analyzer.src.log_analyzer.services.file_reader import read_logs
+
+
+class TestFileReader(unittest.TestCase):
+
+    def setUp(self):
+        self.path = Path(__file__).parent.parent / "resources" / "log_entries_sample.txt"
+
+    def test_read_valid_lines(self):
+        lines = list(read_logs(self.path.as_posix()))
+        self.assertTrue(len(lines) > 0)
+
+    def test_skip_empty_lines(self):
+        lines = list(read_logs(self.path.as_posix()))
+        self.assertFalse("" in lines)
+
+    def test_file_not_found(self):
+        lines = list(read_logs("non_existent_file_path.txt"))
+        self.assertEqual(len(lines), 0)
+
+    def test_skip_lines_with_only_whitespace(self):
+        lines = list(read_logs(self.path.as_posix()))
+        for line in lines:
+            self.assertTrue(line.strip() != "")
+
+    def test_io_error_handling(self):
+        with patch("builtins.open", side_effect=IOError("error")):
+            lines = list(read_logs(self.path.as_posix()))
+            self.assertEqual(lines, [])
